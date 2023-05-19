bcea3cd5ede17ed0c8c32fc3877df86f76b921d5
https://github.com/almostagel <130816893+modabsaeed@users.noreply.github.com>
2023-05-20 01:09:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
-HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+HOST: process.env.HOST === undefined ? '.' : process.env.HOST,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=syJx1bgR#egEoqQr9jTRSMEqZ1jW3Ovye9_B4lgYe9bycEo7WPZg' : process.env.SESSION_ID
 };
